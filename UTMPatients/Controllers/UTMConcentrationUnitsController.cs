3b63b54bfcac7ca560c31785716a84c67e307f73
unknown <ujjwalthapa2307@gmail.com>
2022-01-28 11:03:12 -0500

    COntrollers and Added and Browser title and heading are changed

---
+++ UTMPatients/Controllers/UTMConcentrationUnitsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UTMPatients.Models;
+
+namespace UTMPatients.Controllers
+{
+    public class UTMConcentrationUnitsController : Controller
+    {
+        private readonly PatientsContext _context;
+
+        public UTMConcentrationUnitsController(PatientsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UTMConcentrationUnits
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ConcentrationUnit.ToListAsync());
+        }
+
+        // GET: UTMConcentrationUnits/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var concentrationUnit = await _context.ConcentrationUnit
+                .FirstOrDefaultAsync(m => m.ConcentrationCode == id);
+            if (concentrationUnit == null)
+            {
+                return NotFound();
+            }
+
+            return View(concentrationUnit);
+        }
+
+        // GET: UTMConcentrationUnits/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UTMConcentrationUnits/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ConcentrationCode")] ConcentrationUnit concentrationUnit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(concentrationUnit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(concentrationUnit);
+        }
+
+        // GET: UTMConcentrationUnits/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var concentrationUnit = await _context.ConcentrationUnit.FindAsync(id);
+            if (concentrationUnit == null)
+            {
+                return NotFound();
+            }
+            return View(concentrationUnit);
+        }
+
+        // POST: UTMConcentrationUnits/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("ConcentrationCode")] ConcentrationUnit concentrationUnit)
+        {
+            if (id != concentrationUnit.ConcentrationCode)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(concentrationUnit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConcentrationUnitExists(concentrationUnit.ConcentrationCode))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(concentrationUnit);
+        }
+
+        // GET: UTMConcentrationUnits/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var concentrationUnit = await _context.ConcentrationUnit
+                .FirstOrDefaultAsync(m => m.ConcentrationCode == id);
+            if (concentrationUnit == null)
+            {
+                return NotFound();
+            }
+
+            return View(concentrationUnit);
+        }
+
+        // POST: UTMConcentrationUnits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var concentrationUnit = await _context.ConcentrationUnit.FindAsync(id);
+            _context.ConcentrationUnit.Remove(concentrationUnit);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConcentrationUnitExists(string id)
+        {
+            return _context.ConcentrationUnit.Any(e => e.ConcentrationCode == id);
+        }
+    }
+}
---
+++ UTMPatients/Controllers/UTMDiagnosisCategoriesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UTMPatients.Models;
+
+namespace UTMPatients.Controllers
+{
+    public class UTMDiagnosisCategoriesController : Controller
+    {
+        private readonly PatientsContext _context;
+
+        public UTMDiagnosisCategoriesController(PatientsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UTMDiagnosisCategories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DiagnosisCategory.ToListAsync());
+        }
+
+        // GET: UTMDiagnosisCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diagnosisCategory = await _context.DiagnosisCategory
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (diagnosisCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(diagnosisCategory);
+        }
+
+        // GET: UTMDiagnosisCategories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UTMDiagnosisCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] DiagnosisCategory diagnosisCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(diagnosisCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(diagnosisCategory);
+        }
+
+        // GET: UTMDiagnosisCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diagnosisCategory = await _context.DiagnosisCategory.FindAsync(id);
+            if (diagnosisCategory == null)
+            {
+                return NotFound();
+            }
+            return View(diagnosisCategory);
+        }
+
+        // POST: UTMDiagnosisCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] DiagnosisCategory diagnosisCategory)
+        {
+            if (id != diagnosisCategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(diagnosisCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiagnosisCategoryExists(diagnosisCategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(diagnosisCategory);
+        }
+
+        // GET: UTMDiagnosisCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var diagnosisCategory = await _context.DiagnosisCategory
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (diagnosisCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(diagnosisCategory);
+        }
+
+        // POST: UTMDiagnosisCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var diagnosisCategory = await _context.DiagnosisCategory.FindAsync(id);
+            _context.DiagnosisCategory.Remove(diagnosisCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiagnosisCategoryExists(int id)
+        {
+            return _context.DiagnosisCategory.Any(e => e.Id == id);
+        }
+    }
+}
